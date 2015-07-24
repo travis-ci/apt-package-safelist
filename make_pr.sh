05c034d13677dd4d908f5849c84cb760f05f7568
Hiro Asari <asari.ruby@gmail.com>
2015-07-23 21:28:05 -0400

    Add make_pr.sh [skip ci]
    To make it easier to create a PR when the further analysis
    reveals that the packages are OK to be added.
    
    The checker should say which packages are available in the
    source package.
    All one has to do is run:
    
        ./make_pr.sh apt-package-whitelist 1234
    
    If the file named `package` exists in the same directory,
    its content is read to pick up which packages to add.

---
+++ make_pr.sh
@@ -0,0 +1,106 @@
+#! /usr/bin/env bash
+
+ANSI_RED="\033[31;1m"
+ANSI_GREEN="\033[32;1m"
+ANSI_RESET="\033[0m"
+ANSI_CLEAR="\033[0K"
+
+EXIT_SUCCSS=0
+EXIT_SOURCE_NOT_FOUND=1
+EXIT_SOURCE_HAS_SETUID=2
+
+DEFAULT_BRANCH=master
+
+function notice() {
+	msg=$1
+	echo -e "\n${ANSI_GREEN}${msg}${ANSI_RESET}\n"
+}
+
+function warn() {
+	msg=$1
+	echo -e "\n${ANSI_RED}${msg}${ANSI_RESET}\n"
+}
+
+function usage() {
+	echo "Usage: $0 repo issue_number [package [additional_packages …]]"
+}
+
+if [ $# -lt 2 ]; then
+	usage
+	exit 0
+fi
+
+## Process arguments; nothing funcy
+
+ISSUE_REPO=$1
+shift
+ISSUE_NUMBER=$1
+shift
+
+PACKAGES=( $@ )
+if [ -z "$PACKAGES"  ]; then
+	if [ -f $(dirname $0)/packages ]; then
+		notice "Reading from $(dirname $0)/packages"
+		PACKAGES=$(< $(dirname $0)/packages)
+	else
+		warn "Unable to determine packages to add"
+		usage
+		exit 1
+	fi
+fi
+
+ISSUE_PACKAGE=${PACKAGES[0]}
+
+notice "Setting up PR with\nRepo: ${ISSUE_REPO}\nNUMBER: ${ISSUE_NUMBER}\nPackages: ${PACKAGES[*]}"
+
+BRANCH="apt-package-whitelist-test-${ISSUE_NUMBER}"
+notice "Setting up Git"
+
+if [ -z "`git config --get --global credential.helper`" ]; then
+	notice "set up credential.helper"
+	git config credential.helper "store --file=.git/credentials"
+	notice "https://${GITHUB_OAUTH_TOKEN}:@github.com" > .git/credentials 2>/dev/null
+fi
+if [ -z "`git config --get --global user.email`" ]; then
+	notice "set up user.email"
+	git config --global user.email "contact@travis-ci.com"
+fi
+if [ -z "`git config --get --global user.name`" ]; then
+	notice "set up user.name"
+	git config --global user.name "Travis CI APT package tester"
+fi
+
+notice "Creating commit"
+git checkout $DEFAULT_BRANCH
+git checkout -b $BRANCH
+for p in ${PACKAGES[*]}; do
+	notice "Adding ${p}"
+	env PACKAGE=${p} make add > /dev/null
+done
+git add ubuntu-precise
+git commit -m "Add ${ISSUE_PACKAGE} to ubuntu-precise; resolves travis-ci/${ISSUE_REPO}#${ISSUE_NUMBER}"
+
+COMMIT_EXIT_STATUS=$?
+if [ $COMMIT_EXIT_STATUS -gt 0 ]; then
+	notice "Nothing to commit"
+	exit 0
+fi
+
+notice "Pushing commit"
+
+if [ -z $GITHUB_OAUTH_TOKEN ]; then
+	warn '$GITHUB_OAUTH_TOKEN not set'
+	exit 1
+fi
+git push origin $BRANCH
+git checkout $DEFAULT_BRANCH
+git branch -D $BRANCH
+
+notice "Creating PR"
+COMMENT="For travis-ci/${ISSUE_REPO}#${ISSUE_NUMBER}. adding packages: ${PACKAGES}"
+curl -X POST -sS -H "Content-Type: application/json" -H "Authorization: token ${GITHUB_OAUTH_TOKEN}" \
+	-d "{\"title\":\"Pull request for ${ISSUE_PACKAGE}\",\"body\":\"${COMMENT}\",\"head\":\"${BRANCH}\",\"base\":\"master\"}" \
+	https://api.github.com/repos/travis-ci/apt-package-whitelist/pulls
+curl -X POST -sS -H "Content-Type: application/json" -H "Authorization: token ${GITHUB_OAUTH_TOKEN}" \
+	-d "[\"apt-whitelist-check-run\"]" \
+	${GITHUB_ISSUES_URL}/labels
