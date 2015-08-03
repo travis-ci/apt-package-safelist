82095d82881982bb806a9183c047dd332da3d9bd
Hiro Asari <asari.ruby@gmail.com>
2015-08-03 17:23:05 -0400

    Find curl status correctly [skip ci]

--- make_pr.sh
+++ make_pr.sh
@@ -1,7 +1,5 @@
 #! /usr/bin/env bash
 
-set -o pipefail
-
 ANSI_RED="\033[31;1m"
 ANSI_GREEN="\033[32;1m"
 ANSI_RESET="\033[0m"
@@ -130,11 +128,11 @@ if [ ${has_setuid} -gt 0 ]; then
 fi
 curl -X POST -sS -H "Content-Type: application/json" -H "Authorization: token ${GITHUB_OAUTH_TOKEN}" \
 	-d "{\"title\":\"Pull request for ${ISSUE_PACKAGE}\",\"body\":\"Resolves travis-ci/${ISSUE_REPO}#${ISSUE_NUMBER}.\n${COMMENT}\",\"head\":\"${BRANCH}\",\"base\":\"master\"}" \
-	https://api.github.com/repos/travis-ci/apt-package-whitelist/pulls | tee pr_payload
+	https://api.github.com/repos/travis-ci/apt-package-whitelist/pulls > pr_payload
 if [ $? -eq 0 ]; then
 	curl -X POST -sS -H "Content-Type: application/json" -H "Authorization: token ${GITHUB_OAUTH_TOKEN}" \
-	-d "[\"textual-suid-present\"]" \
-	$(jq .issue_url pr_payload | cut -f2 -d\")/labels
+		-d "[\"textual-suid-present\"]" \
+		$(jq .issue_url pr_payload | cut -f2 -d\")/labels
 fi
 curl -X POST -sS -H "Content-Type: application/json" -H "Authorization: token ${GITHUB_OAUTH_TOKEN}" \
 	-d "[\"apt-whitelist-check-run\"]" \
