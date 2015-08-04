e8cebf1be790b93130fa6de880c99a9b980dcedb
Hiro Asari <asari.ruby@gmail.com>
2015-08-03 23:54:23 -0400

    Check has_setuid value [skip ci]

--- make_pr.sh
+++ make_pr.sh
@@ -129,7 +129,7 @@ fi
 curl -X POST -sS -H "Content-Type: application/json" -H "Authorization: token ${GITHUB_OAUTH_TOKEN}" \
 	-d "{\"title\":\"Pull request for ${ISSUE_PACKAGE}\",\"body\":\"Resolves travis-ci/${ISSUE_REPO}#${ISSUE_NUMBER}.\n${COMMENT}\",\"head\":\"${BRANCH}\",\"base\":\"master\"}" \
 	https://api.github.com/repos/travis-ci/apt-package-whitelist/pulls > pr_payload
-if [ $? -eq 0 ]; then
+if [ $? -eq 0 -a ${has_setuid} -gt 0 ]; then
 	curl -X POST -sS -H "Content-Type: application/json" -H "Authorization: token ${GITHUB_OAUTH_TOKEN}" \
 		-d "[\"textual-suid-present\"]" \
 		$(jq .issue_url pr_payload | cut -f2 -d\")/labels
