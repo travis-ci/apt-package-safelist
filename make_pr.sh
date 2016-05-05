1f63a27694006fd4d9df433fc3d4cec4c2304f18
Hiro Asari <asari.ruby@gmail.com>
2016-05-04 20:05:55 -1000

    Fix issue comment URL

--- make_pr.sh
+++ make_pr.sh
@@ -87,7 +87,7 @@ while [ $current -lt $HITS ]; do
 		echo "This is a duplicate request"
 		curl -X POST -d "{\"body\":\"Duplicate of $ISSUE_REPO#$CANDIDATE_PR_NUMBER\"}" \
 			-H "Content-Type: application/json" -H "Authorization: token ${GITHUB_OAUTH_TOKEN}" \
-			https://api.github.com/repos/travis-ci/$ISSUE_REPO/$ISSUE_NUMBER/comments
+			https://api.github.com/repos/travis-ci/$ISSUE_REPO/issues/$ISSUE_NUMBER/comments
 		curl -X PATCH -d "{\"state\":\"closed\"}" \
 			-H "Content-Type: application/json" -H "Authorization: token ${GITHUB_OAUTH_TOKEN}" \
 			https://api.github.com/repos/travis-ci/$ISSUE_REPO/issues/$ISSUE_NUMBER
